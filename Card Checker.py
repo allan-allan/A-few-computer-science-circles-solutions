120b258b0c062866886f9b68c2e25985652710b5
allan-allan <88670714+allan-allan@users.noreply.github.com>
2021-10-17 14:45:02 +0300

    Update Card Checker.py

--- Card Checker.py
+++ Card Checker.py
@@ -12,13 +12,14 @@ def check(S):
     while len(S) != 19 or  S[4] != ' ' or S[9] != ' ' or S[14] != ' ':  #First condition checked. This returns False if there are no spaces at index 4, 
         return False                                                    #index 9 or index 14 and true if the entered card number conforms to the rules.
 
-    if S_new.isdigit() is False:                                        # Second condition to checkif the card number consists of digits
+    if S_new.isdigit() is False:                                        # Second condition to check if the card number consists of digits
         return False
         
     for i in range (0, len(S_new)):
         x = x + int((S_new)[i])
         i = i + 1
-    if x%10 == 0:                                                        #This checks if the sum of the digits is divisible by 10
+        
+    if x%10 == 0:                                                        # Third condition to check if the sum of the digits is divisible by 10
         return True
     else:
         return False
